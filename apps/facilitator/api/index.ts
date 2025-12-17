6ad8f24c7131d6c45d2447356bf6bb3c48d7e0db
Elvijs Dzirkals <dzirkals@gmail.com>
2025-12-17 13:26:35 +0100

    Add vercel config for website

--- apps/facilitator/api/index.ts
+++ apps/facilitator/api/index.ts
@@ -1,5 +1,8 @@
 import { handle } from "hono/vercel";
-import app from "../src/index";
+import app from "../src/index.js";
+
+export const runtime = "nodejs24.x";
+export const dynamic = "force-dynamic";
 
 // Vercel serverless function handler
 export default handle(app);
